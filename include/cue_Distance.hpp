748e091f1c02ef99f1078da00a03a17e058a0ff6
Joshua Gerrard <enquiries@joshuagerrard.info>
2015-10-29 22:46:54 +0000

    Refactoring
    Moved member function definitions out of class declarations.
    Used braced initialiser syntax.
    Made member functions const.

--- include/cue_Distance.hpp
+++ include/cue_Distance.hpp
@@ -10,13 +10,10 @@ namespace cue
     class Metres : public Unit<StorageType>
     {
     public:
-        static Metres fromMetres (StorageType metres) noexcept
-        { return Metres (metres); }
+        static Metres fromMetres (StorageType metres) noexcept;
 
-        Metres operator+ (Metres rhs) noexcept
-        { return Metres (this->getRawValue() + rhs.getRawValue()); }
-
-        Metres operator+ (Centimetres<StorageType> rhs) noexcept;
+        Metres operator+ (Metres rhs) const noexcept;
+        Metres operator+ (Centimetres<StorageType> rhs) const noexcept;
 
     protected:
         using Unit<StorageType>::Unit;
@@ -26,20 +23,36 @@ namespace cue
     class Centimetres : public Unit<StorageType>
     {
     public:
-        static Centimetres fromCentimetres (StorageType centimetres) noexcept
-        { return Centimetres (centimetres); }
+        static Centimetres fromCentimetres (StorageType centimetres) noexcept;
 
-        Centimetres operator+ (Centimetres rhs) noexcept
-        { return Centimetres (this->getRawValue() + rhs.getRawValue()); }
-
-        Centimetres operator+ (Metres<StorageType> rhs) noexcept
-        { return Centimetres (this->getRawValue() + rhs.getRawValue() * 100); }
+        Centimetres operator+ (Centimetres rhs) const noexcept;
+        Centimetres operator+ (Metres<StorageType> rhs) const noexcept;
 
     protected:
         using Unit<StorageType>::Unit;
     };
 
     template <typename StorageType>
-    Metres<StorageType> Metres<StorageType>::operator+ (Centimetres<StorageType> rhs) noexcept
-    { return Metres<StorageType> (this->getRawValue() + rhs.getRawValue() / StorageType (100)); }
+    Metres<StorageType> Metres<StorageType>::fromMetres (StorageType metres) noexcept
+    { return { metres }; }
+
+    template <typename StorageType>
+    Metres<StorageType> Metres<StorageType>::operator+ (Metres<StorageType> rhs) const noexcept
+    { return { this->getRawValue() + rhs.getRawValue() }; }
+
+    template <typename StorageType>
+    Metres<StorageType> Metres<StorageType>::operator+ (Centimetres<StorageType> rhs) const noexcept
+    { return { this->getRawValue() + rhs.getRawValue() / StorageType (100) }; }
+
+    template <typename StorageType>
+    Centimetres<StorageType> Centimetres<StorageType>::fromCentimetres (StorageType centimetres) noexcept
+    { return { centimetres }; }
+
+    template <typename StorageType>
+    Centimetres<StorageType> Centimetres<StorageType>::operator+ (Centimetres<StorageType> rhs) const noexcept
+    { return { this->getRawValue() + rhs.getRawValue() }; }
+
+    template <typename StorageType>
+    Centimetres<StorageType> Centimetres<StorageType>::operator+ (Metres<StorageType> rhs) const noexcept
+    { return { this->getRawValue() + rhs.getRawValue() * 100 }; }
 }
